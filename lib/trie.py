14dca88e9ee301461d37f41e0d91561d24d08080
nuh.temp <nuh.temp@gmail.com>
2016-10-08 14:45:03 -0700

    add tests for trie

--- lib/trie.py
+++ lib/trie.py
@@ -1,44 +1,46 @@
 class TreeNode(object):
+	"""Representation of a tree's node."""
 
 	def __init__(self, is_leaf=False):
 		self.children = {}
 		self.is_leaf = is_leaf
 
 
-
 class PrefixTree(object):
+    """Implementation of prefix tree."""
 
-	def __init__(self):
-		self.root = TreeNode()
+    def __init__(self):
+        self.root = TreeNode()
 
-	def Insert(self, value):
-		n = self.root
-		for c in value:
-			if c not in n.children:
-				new_node = TreeNode()
-				n.children[c] = new_node
-			n = n.children[c]
-		n.is_leaf = True
+    def Insert(self, value):
+        """Inserts word into a tree."""
+        node = self.root
+        for char in value:
+            if char not in node.children:
+                new_node = TreeNode()
+                node.children[char] = new_node
+            node = node.children[char]
+        node.is_leaf = True
 
+    def _Traverse(self, string, node, results):
+        """Traverses tree recursively from given node."""
+        if node.is_leaf:
+            results.append(string)
+            return
 
-	def _traverse(string, node, results):
-		if node.is_leaf:
-			results.append(string)
-			return
+        for char, node in node.children.iteritems():
+            self._Traverse(string + char, node, results)
 
-		for c, n in node.children.iteritems():
-			_traverse(string + c, n, results)
+    def SearchByPrefix(self, value):
+        """Searches for for all words started with given prefix."""
+        results = []
+        node = self.root
+        for char in value:
+            if char not in node.children:
+                return results
+            else:
+                node = node.children[char]
 
-
-	def Search(self, value):
-		n = self.root
-		for c in value:
-			if c not in n:
-				return False
-			else:
-				n = n.children
-
-		# value is in tree
-		results = []
-		traverse(value, n, results)
-		return results
+        # value is in tree
+        self._Traverse(value, node, results)
+        return results
--- lib/trie_test.py
+++ lib/trie_test.py
@@ -1,11 +1,36 @@
 import unittest
-import utils
+import trie
 
 
 class TreeTest(unittest.TestCase):
 
-	def testInsert(self):
-		t = utils.PrefixTree()
-		t.Insert('ABC')
+    def testInsert(self):
+        t = trie.PrefixTree()
+        word = 'ABC'
+        t.Insert(word)
+        node = t.root
+        for char in word:
+            self.assertIn(char, node.children)
+            node = node.children[char]
 
-unittest.main()
+    def testSearchByPrefix(self):
+        t = trie.PrefixTree()
+        t.Insert('ABC')
+        t.Insert('ABB')
+        t.Insert('ADC')
+        t.Insert('AAB')
+        actual = t.SearchByPrefix('AB')
+        self.assertItemsEqual(actual, ['ABC', 'ABB'])
+
+    def testSearchByPrefixNoResults(self):
+        t = trie.PrefixTree()
+        t.Insert('ABC')
+        t.Insert('ABB')
+        t.Insert('ADC')
+        t.Insert('AAB')
+        actual = t.SearchByPrefix('XXX')
+        self.assertItemsEqual(actual, [])
+
+
+if __name__ == "__main__":
+    unittest.main()
